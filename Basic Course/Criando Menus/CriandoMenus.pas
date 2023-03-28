c8980e93c3145725d6e1f08338a34e21810fcfd4
Felipe Velho Schio <fvschio@gmail.com>
2023-03-28 10:19:03 -0300

    Add files via upload

---
+++ Basic Course/Criando Menus/CriandoMenus.pas
@@ -0,0 +1,38 @@
+unit CriandoMenus;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus;
+
+type
+  TForm1 = class(TForm)
+    MainMenu1: TMainMenu;
+    Sistema1: TMenuItem;
+    Configuraes1: TMenuItem;
+    Usurios1: TMenuItem;
+    N1: TMenuItem;
+    Sair1: TMenuItem;
+    Cadastros1: TMenuItem;
+    Clientes1: TMenuItem;
+    Produtos1: TMenuItem;
+    Consultas1: TMenuItem;
+    Clientes2: TMenuItem;
+    Produtos2: TMenuItem;
+    CriarNovoUsurio1: TMenuItem;
+    rocardeUsurio1: TMenuItem;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
